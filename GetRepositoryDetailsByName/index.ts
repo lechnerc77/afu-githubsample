77304c448a294cf6177b3d5b01a13e7f50c5fff8
Christian Lechner <lechnerc77@users.noreply.github.com>
2020-12-21 13:03:45 +0100

    sample retry scenario

--- GetRepositoryDetailsByName/index.ts
+++ GetRepositoryDetailsByName/index.ts
@@ -3,6 +3,17 @@ import { Octokit } from "@octokit/core"
 
 const activityFunction: AzureFunction = async function (context: Context): Promise<string> {
 
+    if (context.bindingData.raiseException && context.bindingData.raiseException === true) {
+        context.log("Error was enforced by caller")
+        throw {
+            name: "ForcedException",
+            message: "Caller enforced exception",
+            toString: function () {
+                return this.name + ": " + this.message;
+            }
+        }
+    }
+
     const octokit = new Octokit()
 
     const query = `${context.bindingData.repositoryName.toString()} in:name`
--- GitHubInfoOrchestrator/index.ts
+++ GitHubInfoOrchestrator/index.ts
@@ -2,11 +2,25 @@
 
 const orchestrator = df.orchestrator(function* (context) {
 
-    const userId:string = yield context.df.callActivity("GetRepositoryDetailsByName", context.bindingData.input)
-    
+    const firstRetryIntervalInMilliseconds: number = 1000
+    const maxNumberOfAttempts: number = 3
+    const maxRetryIntervalInMilliseconds: number = 1000
+    const retryTimeoutInMilliseconds: number = 7000
+
+    const retryConfig: df.RetryOptions = new df.RetryOptions(firstRetryIntervalInMilliseconds, maxNumberOfAttempts)
+    retryConfig.maxRetryIntervalInMilliseconds = maxRetryIntervalInMilliseconds
+    retryConfig.retryTimeoutInMilliseconds = retryTimeoutInMilliseconds
+
+    // For demo purposes
+    if (context.df.isReplaying == true) {
+        context.bindingData.input.raiseException = false
+    }
+
+    const userId: string = yield context.df.callActivityWithRetry("GetRepositoryDetailsByName", retryConfig, context.bindingData.input)
+
     context.bindingData.input.userId = userId
 
-    const userInfos = yield context.df.callActivity("GetUserDetailsById", context.bindingData.input)
+    const userInfos = yield context.df.callActivityWithRetry("GetUserDetailsById", retryConfig, context.bindingData.input)
 
     return userInfos
 })
